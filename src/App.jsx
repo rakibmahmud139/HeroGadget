cb5dacbfa1eae211630298867d49aa2563ee50ab
Rakibmahmud <hasansaikat74@gmail.com>
2023-06-28 21:17:52 +0600

    added first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { Outlet } from "react-router-dom"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+
 const App = () => {
-  return <h1>Welcome to HeroGadget</h1>
+  return <div>
+    <Header />
+    <div className="min-h-[calc(100vh-137px)]">
+      <Outlet />
+    </div>
+    <Footer />
+  </div>
+
 }
 
 export default App
